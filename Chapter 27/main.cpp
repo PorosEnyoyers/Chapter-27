1c441b961706967589d29bb85ef403b05a4e4114
Poros Enjoyers <138520882+PorosEnyoyers@users.noreply.github.com>
2025-12-27 11:05:20 -0800

    1

--- Chapter 27/main.cpp
+++ Chapter 27/main.cpp
@@ -466,102 +466,100 @@
 //    return 0;
 //}
 
+//#include <iostream>
+//#include <utility>
+//#include <stdexcept> 
+//
+//class MoveClass
+//{
+//private:
+//    int* m_resource{};
+//
+//public:
+//    MoveClass() = default;
+//
+//    MoveClass(int resource)
+//        : m_resource{ new int{ resource } }
+//    {
+//    }
+//
+//    MoveClass(const MoveClass& that)
+//    {
+//
+//        if (that.m_resource != nullptr)
+//        {
+//            m_resource = new int{ *that.m_resource };
+//        }
+//    }
+//
+//    MoveClass(MoveClass&& that) noexcept
+//        : m_resource{ that.m_resource }
+//    {
+//        that.m_resource = nullptr;
+//    }
+//
+//    ~MoveClass()
+//    {
+//        std::cout << "destroying " << *this << '\n';
+//
+//        delete m_resource;
+//    }
+//
+//    friend std::ostream& operator<<(std::ostream& out, const MoveClass& moveClass)
+//    {
+//        out << "MoveClass(";
+//
+//        if (moveClass.m_resource == nullptr)
+//        {
+//            out << "empty";
+//        }
+//        else
+//        {
+//            out << *moveClass.m_resource;
+//        }
+//
+//        out << ')';
+//
+//        return out;
+//    }
+//};
+//
+//
+//class CopyClass
+//{
+//public:
+//    bool m_throw{};
+//
+//    CopyClass() = default;
+//
+//    CopyClass(const CopyClass& that)
+//        : m_throw{ that.m_throw }
+//    {
+//        if (m_throw)
+//        {
+//            throw std::runtime_error{ "abort!" };
+//        }
+//    }
+//};
+//
+//int main()
+//{
+//    char strBuf[100]{};
+//    std::cin.getline(strBuf, 100);
+//    std::cout << strBuf << '\n';
+//    std::cout << std::cin.gcount() << " characters were read" << '\n';
+//
+//    return 0;
+//}
+
+#include <string>
 #include <iostream>
-#include <utility>
-#include <stdexcept> 
-
-class MoveClass
-{
-private:
-    int* m_resource{};
-
-public:
-    MoveClass() = default;
-
-    MoveClass(int resource)
-        : m_resource{ new int{ resource } }
-    {
-    }
-
-    MoveClass(const MoveClass& that)
-    {
-
-        if (that.m_resource != nullptr)
-        {
-            m_resource = new int{ *that.m_resource };
-        }
-    }
-
-    MoveClass(MoveClass&& that) noexcept
-        : m_resource{ that.m_resource }
-    {
-        that.m_resource = nullptr;
-    }
-
-    ~MoveClass()
-    {
-        std::cout << "destroying " << *this << '\n';
-
-        delete m_resource;
-    }
-
-    friend std::ostream& operator<<(std::ostream& out, const MoveClass& moveClass)
-    {
-        out << "MoveClass(";
-
-        if (moveClass.m_resource == nullptr)
-        {
-            out << "empty";
-        }
-        else
-        {
-            out << *moveClass.m_resource;
-        }
-
-        out << ')';
-
-        return out;
-    }
-};
-
-
-class CopyClass
-{
-public:
-    bool m_throw{};
-
-    CopyClass() = default;
-
-    CopyClass(const CopyClass& that)
-        : m_throw{ that.m_throw }
-    {
-        if (m_throw)
-        {
-            throw std::runtime_error{ "abort!" };
-        }
-    }
-};
 
 int main()
 {
-    std::pair my_pair{ CopyClass{}, MoveClass{13} };
+	std::string strBuf{};
+	std::getline(std::cin, strBuf);
+	std::cout << strBuf << '\n';
 
-    std::cout << "my_pair.second: " << my_pair.second << '\n';
-
-    try
-    {
-        my_pair.first.m_throw = true;
-
-        std::pair moved_pair{ std::move(my_pair) };
-
-        std::cout << "moved pair exists\n";
-    }
-    catch (const std::exception& ex)
-    {
-        std::cerr << "Error found: " << ex.what() << '\n';
-    }
-
-    std::cout << "my_pair.second: " << my_pair.second << '\n';
-
-    return 0;
+	return 0;
 }
